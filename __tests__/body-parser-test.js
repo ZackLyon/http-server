58521d1a7d332b190144dbf0cf3ea183072cf0cb
Zack Lyon <zack.j.lyon@gmail.com>
2021-11-01 18:07:59 -0700

    bodyParser func throws error if incorrect content type and passing test

--- __tests__/body-parser-test.js
+++ __tests__/body-parser-test.js
@@ -3,8 +3,26 @@ const bodyParser = require('../lib/body-parser.js');
 describe('body parser', () => {
   it('should return null if the method is not POST, PUT, or PATCH', async () => {
     const request = { method: 'GET' };
-    const actual = bodyParser(request);
+    const actual = await bodyParser(request);
     const expected = null;
+
     expect(actual).toEqual(expected);
+  });
+
+  it('should throw error if content type is not application/json', async () => {
+    const request = {
+      method: 'POST',
+      headers: {
+        'content-type': 'text/html',
+      },
+    };
+
+    const expected = 'Incorrect content type. Must be application/json.';
+
+    try {
+      await bodyParser(request);
+    } catch (err) {
+      expect(err).toEqual(expected);
+    }
   });
 });
